d4b4a524069d98f41c7457841378d1231a68639f
Mohammed Allibalogun <mallibalogun18@gmail.com>
2023-10-10 10:21:42 -0400

    Brewery Python Challenge.

--- HelloWorld.py
+++ HelloWorld.py
@@ -13,5 +13,10 @@ print("""Forward Slash (/)""")
 # input function
 name = input("What is your name?\n")
 age = input("How old are you?\n")
-
 print('Your name is '+name+" and you are "+age+" years old!!")
+input("Your last name is "+input("What is your last name?\n")+".")
+print(input('What are you learning today?'))
+print(input("What's your name?")+" "+input("What's your last name?"))
+
+
+
---
+++ brewery.py
@@ -0,0 +1,4 @@
+# Python Beer Challenge
+print("Day 1 Python Challenge")
+print("Welcome to the Brewery")
+print("The name of your beer is "+input('Enter the first word...\n')+" "+input('Enter the second word...\n')+"!!")
