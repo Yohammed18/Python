f0a810a9184d15329b8def0dda6f15aca36bdaa6
Mohammed Allibalogun <mallibalogun18@gmail.com>
2023-10-10 09:50:47 -0400

    Python basic i.e. input()

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,17 @@
 print('Hello World')
+print('Learning with \'TOTAL Python\' is super fun!')
+print('500+55')
+print(500 + 55)
+print("Line 1\nLine 2\nLine 3")
+print('A\tB\tC')
+print('D\tE\tF')
+print('G\tH\tI')
+print("""Backslash (\)""")
+print("""Forward Slash (/)""")
+
+
+# input function
+name = input("What is your name?\n")
+age = input("How old are you?\n")
+
+print('Your name is '+name+" and you are "+age+" years old!!")
